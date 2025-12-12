7e345bb19ffc50f1fe97e1aa06f9b207241cab05
ahmed <ahmed.adnan0202@gmail.com>
2025-12-12 19:20:12 +0300

    chnaged port to 3300

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+// vite.config.js
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
@@ -5,4 +7,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // ------------------------------------
+  // Add the 'server' configuration here:
+  server: {
+    port: 3300, // <--- This sets the development server port
+  },
+  // ------------------------------------
 });
